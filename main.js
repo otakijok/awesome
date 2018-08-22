0cc01cd221eca706cbbdcb63606fe8aa16cfe5c7
otakijok <suprkoji@gmail.com>
2018-08-23 00:34:08 +0900

    追加します。

---
+++ main.js
@@ -0,0 +1,11 @@
+var $ = require('jquery');
+var moment = require('moment');
+
+$(function() {
+    var $msg = $('#msg');
+    $msg.fadeOut('slow', function() {
+        $msg.text("webpack " + 
+                moment().format("YYYY-MM-DD HH:mm:ss"))
+            .fadeIn('slow');
+    });
+});
